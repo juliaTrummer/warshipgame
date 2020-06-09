9e86af949ca60dc9ddefd3f19414baa82c170a01
truju <julia.trummer@edu.fh-joanneum.at>
2020-06-09 17:06:59 +0200

    build fail

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var clients = [ ];
 
 app.use(bodyParser.json());
 
-const db = require('./app/config/db.config.js');
-const User = db.users;
-
 server.get("/", function (req, res) {
     res.render("index");
 })
@@ -99,37 +96,3 @@ wss.broadCastSender = function(data, sender){
         }
     }) 
 }
-
-
-
-
-
-
-
-/*
-// force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-    console.log('Drop and Resync with { force: true }');
-    initial();
-});
-require('./app/route/user.route.js')(app);
-
-// Create a Server
-var server = app.listen(process.env.PORT || 5000, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("App listening at http://%s:%s", host, port)
-})
-
-function initial() {
-    User.create({
-        username: "NONAME"
-    });
-
-    User.create({
-        username: "JULIA"
-    });
-}
-*/
