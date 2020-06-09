86f39ff2128fccac1e4615ac560c75cbd9c37253
truju <julia.trummer@edu.fh-joanneum.at>
2020-06-09 18:03:00 +0200

    fixed build fail

--- server.js
+++ server.js
@@ -11,15 +11,12 @@ app.use(bodyParser.json());
 const db = require('./app/config/db.config.js');
 const User = db.users;
 
-server.get("/", function (req, res) {
-    res.render("index");
-})
+app.use(express.static('client'));
 
-server.listen(port, function(){
-    console.log('Server is now listening to: ', port)
-})
+app.use(express.static(__dirname + "/"));
 
-const wsServer = new Server({server})
+var server = http.createServer(app);
+server.listen(port);
 
 console.log("Server listening on port: %d", port);
 
@@ -99,37 +96,3 @@ wss.broadCastSender = function(data, sender){
         }
     })
 }
-
-
-
-
-
-
-
-/*
-// force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-    console.log('Drop and Resync with { force: true }');
-    initial();
-});
-require('./app/route/user.route.js')(app);
-
-// Create a Server
-var server = app.listen(process.env.PORT || 5000, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("App listening at http://%s:%s", host, port)
-})
-
-function initial() {
-    User.create({
-        username: "NONAME"
-    });
-
-    User.create({
-        username: "JULIA"
-    });
-}
-*/
