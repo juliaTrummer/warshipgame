f5f4e9ccda761763900d34a4f062bdf95d18505c
truju <julia.trummer@edu.fh-joanneum.at>
2020-06-13 23:11:00 +0200

    redid db functions

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const getData = require('./server/db_data/get');
+const postData = require('./server/db_data/post');
+const deleteData = require('./server/db_data/delete');
+const putData = require('./server/db_data/put');
 var WebSocketServer = require("websocket").server;
 var http = require("http");
 var express = require('express');
@@ -10,6 +14,44 @@ var currentPlayer = ""
 app.use(bodyParser.json());
 app.use(express.static('client'));
 app.use(express.static(__dirname + "/"));
+
+/*
+Gets Data from DB
+needs value = battleshipUsers | generatedShipFields | usedFields
+get('battleshipUsers')
+ */
+function get(tableName){
+    const data = getData(tableName);
+}
+
+/*
+Inserts a new value to Database
+needs tablename and value to insert
+EXAMPLE: post('battleshipUsers', 'Julia');
+https://restfulapi.net/rest-put-vs-post/
+ */
+function post(tableName, value){
+    const data = postData(tableName, value);
+}
+
+/*
+Updates Data in Database
+needs tableName, value1 (to be set), column1(to be set), value2(to be overridden), column2 (wich column should be overriden)
+EXAMPLE: put ('battleshipUsers', 'Lea', 'userName', 'Julia', 'userName');
+https://restfulapi.net/rest-put-vs-post/
+ */
+function put(tableName, val1, col1, val2, col2){
+    const data = putData(tableName, val1, col1, val2, col2);
+}
+
+/*
+Deletes Table content
+needs tableName
+EXAMPLE: clear('battleshipUsers');
+ */
+function clear (tableName){
+    const data = deleteData(tableName);
+}
 
 var server = http.createServer(app);
 server.listen(port);
