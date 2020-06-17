dafa684c0d1733acb5fd8c0580de3e59001ad3f3
truju <julia.trummer@edu.fh-joanneum.at>
2020-06-17 20:11:15 +0200

    fixed DB Query PUT

--- server/db_data/put.js
+++ server/db_data/put.js
@@ -2,7 +2,7 @@ const dbQuery = require('../helper/db.js');
 
 function put(value1, value2) {
     if(value1 !== undefined && value1 != null && value2 !== undefined && value2 != null) {
-        var sql =  'UPDATE public.\"battleshipUsers\" SET \"userName\" = ($1) WHERE \"clientid\" = ($2);';
+        var sql =  'UPDATE public.\"battleshipUsers\" SET \"userName\" = ($1) WHERE \"clientID\" = ($2);';
         var values = [value1, value2];
         console.log(sql, values);
         dbQuery(sql, values)
