b109a7857c90d96e25edf08c07f41f10f19ddc2a
truju <julia.trummer@edu.fh-joanneum.at>
2020-06-17 20:39:16 +0200

    db connection

--- server/helper/db.js
+++ server/helper/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'ykpdmzzslhfxng',
-    host: 'ec2-54-75-231-215.eu-west-1.compute.amazonaws.com',
-    database: 'd73ocv2t8c020',
-    password: '02bf0f545f9b0acc26e130bcc4fb84ac1ce7fbc72c773b981569cdeaa709d679',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     },
